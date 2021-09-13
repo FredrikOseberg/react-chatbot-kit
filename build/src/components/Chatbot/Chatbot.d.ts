81edbefce4c466dc18a59fc2fcbb9b27dd45c632
Fredrik Oseberg <fredrik.no@gmail.com>
2021-09-14 00:27:14 +0200

    fix: update deps

--- build/src/components/Chatbot/Chatbot.d.ts
+++ build/src/components/Chatbot/Chatbot.d.ts
@@ -3,11 +3,11 @@ interface IChatbotProps {
     actionProvider: any;
     messageParser: any;
     config: IConfig;
-    headerText: string;
-    placeholderText: string;
-    saveMessages: (ref: any) => any;
-    messageHistory: () => any;
-    validator: (input: string) => Boolean;
+    headerText?: string;
+    placeholderText?: string;
+    saveMessages?: (ref: any) => any;
+    messageHistory?: () => any;
+    validator?: (input: string) => Boolean;
 }
 declare const Chatbot: ({ actionProvider, messageParser, config, headerText, placeholderText, saveMessages, messageHistory, validator, ...rest }: IChatbotProps) => JSX.Element;
 export default Chatbot;
